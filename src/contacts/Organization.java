b3c8320ea16e19d2c1abc0730527e624feb12b91
dawids21 <dawid.stasiak21@gmail.com>
2020-08-03 13:22:55 +0200

    Add fields and constructors to Organization

--- src/contacts/Organization.java
+++ src/contacts/Organization.java
@@ -1,5 +1,18 @@
 package contacts;
 
-public class Organization {
+public class Organization extends Contact {
 
+    private final String name;
+    private final String address;
+
+    public Organization(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public Organization(String phoneNumber, String name, String address) {
+        super(phoneNumber);
+        this.name = name;
+        this.address = address;
+    }
 }
