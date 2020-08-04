4a992c58d4eddd7c8e3b525c04f06caa731a5ba1
dawids21 <dawid.stasiak21@gmail.com>
2020-08-04 07:54:34 +0200

    Remove editRecord method from ListOfContacts

--- src/contacts/ListOfContacts.java
+++ src/contacts/ListOfContacts.java
@@ -18,64 +18,6 @@ public class ListOfContacts {
         return records.get(index);
     }
 
-    public void editRecord(int index, String field, String value) { //TODO
-        if (index < 0 || index > records.size()) {
-            System.out.println("There is no such record");
-            return;
-        }
-
-        var record = records.get(index);
-        if (record instanceof Person) {
-            var person = (Person) record;
-
-            switch (field) {
-                case "name":
-                    person.setName(value);
-                    System.out.println("The record updated!");
-                    break;
-                case "surname":
-                    person.setSurname(value);
-                    System.out.println("The record updated!");
-                    break;
-                case "number":
-                    person.setPhoneNumber(value);
-                    if (!value.equals("") && person.getPhoneNumber()
-                                                   .equals("")) {
-                        System.out.println("Wrong number format!");
-                    }
-                    System.out.println("The record updated!");
-                    break;
-                default:
-                    System.out.println("There is no such field in record");
-                    break;
-            }
-        } else if (record instanceof Organization) {
-            var organization = (Organization) record;
-
-            switch (field) {
-                case "name":
-                    organization.setName(value);
-                    System.out.println("The record updated!");
-                    break;
-                case "address":
-                    organization.setAddress(value);
-                    System.out.println("The record updated!");
-                    break;
-                case "number":
-                    organization.setPhoneNumber(value);
-                    if (!value.equals("") && organization.getPhoneNumber()
-                                                         .equals("")) {
-                        System.out.println("Wrong number format!");
-                    }
-                    System.out.println("The record updated!");
-                    break;
-                default:
-                    System.out.println("There is no such field in record");
-                    break;
-            }
-        }
-    }
-
     public void removeRecord(int index) {
         if (index >= records.size()) {
             System.out.println("There is no such record");
