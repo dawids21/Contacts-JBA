351cd296197cfab0e84b93308c7e76331cc1b44e
dawids21 <dawid.stasiak21@gmail.com>
2020-08-04 08:37:06 +0200

    Implement getInfo in person

--- src/contacts/Person.java
+++ src/contacts/Person.java
@@ -88,6 +88,17 @@ public class Person extends Contact {
     }
 
     @Override
+    public void getInfo() {
+        System.out.println("Name: " + getName());
+        System.out.println("Surname: " + getSurname());
+        System.out.println("Birth date: " + (getBirthDate() != null ? getBirthDate() : "[no data]"));
+        System.out.println("Gender: " + getGender());
+        System.out.println("Number: " + (hasNumber() ? getPhoneNumber() : "[no data]"));
+        System.out.println("Time created: " + getTimeCreated());
+        System.out.println("Time last edit: " + getTimeEdited());
+    }
+
+    @Override
     public String toString() {
         return getName() + " " + getSurname();
     }
