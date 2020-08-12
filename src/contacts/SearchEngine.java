6b319bde3cb8f9347b85102dcf936faf6035809d
dawids21 <dawid.stasiak21@gmail.com>
2020-08-12 07:56:27 +0200

    Write class to search for contacts

---
+++ src/contacts/SearchEngine.java
@@ -0,0 +1,32 @@
+package contacts;
+
+public class SearchEngine {
+
+    private final ListOfContacts input;
+
+    private ListOfContacts results = null;
+
+    public SearchEngine(ListOfContacts input) {
+        this.input = input;
+    }
+
+    public void search(String query) {
+        results = new ListOfContacts();
+        for (var contact : input) {
+            if (contact.toString()
+                       .toLowerCase()
+                       .contains(query.toLowerCase())) {
+                results.add(contact);
+            }
+        }
+    }
+
+    public void print() {
+        System.out.println("Found " + results.size() + " results:");
+        results.print();
+    }
+
+    public Contact get(int index) {
+        return results.get(index);
+    }
+}
